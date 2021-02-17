a973380a9b8803990d68ca773986d0107a9ad8c2
unknown <gokhanates92@gmail.com>
2021-02-17 15:10:59 +0300

    mocha test script

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+//Her descrip içinde birden fazla it() olabilir.Bunların içinde test yapabiliriz.
+describe('Node Server',() => {
+   it('(GET/) anasayfayı döndürür',(done) => {
+      chai.request(server)
+          .get('/')
+          .end((err,res)=>{
+             res.should.have.status(200);
+             done();
+          });
+       //done(); //done demek test bitti herşey yolunda.
+   });
+});
